c47991f49c9b367aa30fd0dacb233b4ac0c47a67
Akanksha Sawant <157445375+AkankshaASawant@users.noreply.github.com>
2024-09-24 21:45:32 +0530

    Time: 9 ms (90.47%), Space: 11.6 MB (95.78%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s): 
+        stack = [] # To keep track of opening brackets
+        closeToOpen = {')': '(', ']': '[', '}': '{'} 
+        
+        for c in s: 
+            if c in closeToOpen: 
+                if stack and stack[-1] == closeToOpen[c]: 
+                    stack.pop()
+                else: 
+                    return False
+            else: 
+                stack.append(c)
+                
+        return True if not stack else False        
+        
